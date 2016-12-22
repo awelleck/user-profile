c37e7460755d2f2519bab858321dc49a0da8845a
Andrew Welleck <andrew8838@gmail.com>
2016-12-21 22:59:55 -0600

    Work in progress towards communication with DB

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/[practice]'
+db = SQLAlchemy(app)
 
 # decorator for index
 @app.route("/")
 def index():
-    return "Index"
+    return render_template("index.html")
 
-@app.route("/<username>")
+# decorator for each profile page
+@app.route("/profile/<username>")
 def profile(username):
-	return "Hello %s" % username
+	return render_template("profile.html", username=username)
+
+@app.route("/db", method=['PUT'])
+def to_db():
+	print("PUT to db")
 
 if __name__ == "__main__":
     app.run(debug=True)
