47cbcfdae9d19a36b9cc6cdd8a643182fae43e9a
Andrew Welleck <andrew8838@gmail.com>
2017-02-07 20:12:16 -0600

    Adding basic SQL schema for table setup

---
+++ sql/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS entries;
+CREATE SEQUENCE entries_id_seq;
+CREATE TABLE entries (
+  id integer PRIMARY KEY DEFAULT nextval('entries_id_seq'),
+  username varchar(80) UNIQUE,
+  password varchar(100),
+  email varchar(25) UNIQUE,
+  first_name varchar(25),
+  last_name varchar(25)
+);
+ALTER SEQUENCE entries_id_seq OWNED BY entries.id;
