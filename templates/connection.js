7d5fbe55efacfd3d9ac981f3cb1de944d8f7237f
Andrew Welleck <andrew8838@gmail.com>
2017-02-23 19:57:44 -0600

    Front end redo of register.html

--- templates/connection.js
+++ templates/connection.js
@@ -1,23 +1,23 @@
 $(document).ready(function() {
-	var socket = io.connect('http://' + document.domain + ':' + location.port);
+  var socket = io.connect('http://' + document.domain + ':' + location.port);
 
-	var offset = new Date().getTimezoneOffset();
-	var second = '{{ second }}';
-	console.log('Offset in minutes: ' + offset);
-	hours = offset / 60
-	console.log('Offest in hours: ' + hours)
+  var offset = new Date().getTimezoneOffset();
+  var second = '{{ second }}';
+  console.log('Offset in minutes: ' + offset);
+  var hours = offset / 60
+  console.log('Offest in hours: ' + hours)
 
-	socket.on('connect', function() {
-		socket.send('User has connected!');
-	});
+  socket.on('connect', function() {
+    socket.send('User has connected!');
+  });
 
-	socket.on('message', function(msg) {
-		$("#messages").append('<li>' + msg + '</li>');
-		console.log('Received message!');
-	});
+  socket.on('message', function(msg) {
+    $("#messages").append('<li>' + msg + '</li>');
+    console.log('Received message!');
+  });
 
-	$('#submit').on('click', function() {
-		socket.send($('#input').val());
-		$('#input').val('');
-	});
+  $('#submit').on('click', function() {
+    socket.send($('#input').val());
+    $('#input').val('');
+  });
 });
